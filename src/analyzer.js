55091ff9c21a8006e2decfe1cfcb3e58bc8e5b2a
CaroMV <carolina.moreno.v@ug.uchile.cl>
2023-07-14 12:42:54 -0400

    Comentarios de contenidos mínimos y html según estos

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,64 @@
 const analyzer = {  
   getWordCount: (text) => {
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+ // Dividir string por " " (para capturar palabras)
+  // Contar el Número de strings
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getCharacterCountExcludingSpaces: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getAverageWordLength: (text) => {    
     //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getNumberCount: (text) => {
     //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
   },
+
   getNumberSum: (text) => {
     //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
   },
+
 };
 
 export default analyzer;
+
+
+//JavaScript
+//Tipos de datos primitivos
+
+//La aplicación convierte valores tipo string a tipo number.
+//Strings (cadenas de caracteres)
+
+//La aplicación usa métodos para manipular strings como split, trim o replace.
+//Variables (declaración, asignación, ámbito)
+
+//La aplicación declara variables con let y const.
+
+//La aplicación NO declara variables con var.
+
+//Uso de condicionales (if-else, switch, operador ternario, lógica booleana)
+
+//La aplicación usa el statement if..else para evaluar condiciones.
+//Uso de bucles/ciclos (while, for, for..of)
+
+//La aplicación usa el statement for para crear un bucle.
+//Funciones (params, args, return)
+
+//El objeto analyzer contiene un método getWordCount para calcular el recuento de palabras de un texto.
+
+//El objeto analyzer contiene un método getCharacterCount para calcular el recuento de caracteres de un texto.
+
+//El objeto analyzer contiene un método getCharacterCountExcludingSpaces para calcular el recuento de caracteres excluyendo espacios y signos de puntuación de un texto.
+
+//El objeto analyzer contiene un método getNumbersCount para contar cúantos números hay en un texto.
+
+//El objeto analyzer contiene un método getNumbersSum para la suma longitud media de los números en un texto.
+
+//El objeto analyzer contiene un método getAverageWordLength para calcular la longitud media de las palabras en un texto.
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import analyzer from './analyzer.js';
-
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+// Uso de selectores del DOM
+
+// La aplicación usa el selector del DOM querySelector.
+
+// La aplicación usa el selector del DOM getElementById.
+
+//Manejo de eventos del DOM (listeners, propagación, delegación)
+
+// La aplicación registra un Event Listener para escuchar el evento keyup del <textarea> para actualizar las métricas cuando se haga escriba en el cuadro de texto.
+
+// La aplicación registra un Event Listener para escuchar el evento click del <button> que limpia el contenido de la caja de texto.
+
+//Manipulación dinámica del DOM
+
+//La aplicación actualiza el atributo textContent o innerHTML de los <li> que mostrar las métricas del texto.
+
+
